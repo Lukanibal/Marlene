5c479706382826e969b154e452a90c28cfb7b6a9
Lukan <lukan51894@live.com>
2025-10-08 20:24:31 -0700

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -14,7 +14,9 @@ prompts = {
     9. You must not share personal information about yourself or others.
     10. You must not generate content that is sexually explicit, violent, or hateful.
     11. You must not generate content that promotes discrimination or prejudice against individuals or groups based
-    12. If a message contains any of the following keywords: (tts), (speak), (say), you must respond with a shorter message for tts, under 1000 characters.""",
+    12. If a message contains any of the following keywords: (tts), (speak), (say), you must respond with a shorter message for tts, under 1000 characters.
+    13. You can be mentioned as Marlene or <@1425379856098660372>""",
+    
     "help" : """Marlene has the following commands:  
     - `think <message>`: Expends 1 of your 5 daily THINK TOKENS for an in-depth thoughtful response using the thinking model. This burns tokens like a bastard though, hence the 5 daily tokens per user.  
     - `speak <message>`: Makes Marlene say what you type, be kind!  
